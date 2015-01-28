0c52911bfb6ca0229d7308f402e5abb96326fa5b
Zewa <zewa666@gmail.com>
2015-01-28 22:43:08 +0100

    * fixed issue with breaking changes in Yeoman api

--- app/index.js
+++ app/index.js
@@ -1,26 +1,29 @@
 var fs = require('fs');
 var path = require('path');
+var generators = require('yeoman-generator');
 
-var Generator = module.exports = function() {
-  var prompts = [];
-  var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
-  var ignores = [
-    '.git',
-    'LICENSE',
-    'README.md',
-  ];
+var Generator = module.exports = generators.Base.extend({
+  installAurelia: function() {
+    var prompts = [];
+    var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+    var ignores = [
+      '.git',
+      'LICENSE',
+      'README.md',
+    ];
 
-  this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 
-  this.log.writeln('Generating from ' + 'Aurelia Skeleton App'.cyan + ' v' + this.package.version.cyan + '...');
+    this.log.writeln('Generating from ' + 'Aurelia Skeleton App'.cyan + ' v' + this.package.version.cyan + '...');
 
-  files.forEach(function(file) {
-    if (ignores.indexOf(file) !== -1) {
-      return;
-    }
+    files.forEach(function(file) {
+      if (ignores.indexOf(file) !== -1) {
+        return;
+      }
 
-    this.copy(file, file);
-  }, this);
-};
+      this.copy(file, file);
+    }, this);
+  }
+});
 
 Generator.name = "Generator Aurelia";
