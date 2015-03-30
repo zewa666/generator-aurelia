ea6512a7ad662e5b888f46deeebb670ed369bfd6
zewa666 <zewa666@gmail.com>
2015-03-30 11:09:47 +0200

    * fixed gitmodules path * added automatic npm install

--- app/index.js
+++ app/index.js
@@ -3,25 +3,27 @@ var path = require('path');
 var generators = require('yeoman-generator');
 
 var Generator = module.exports = generators.Base.extend({
-  init: function() {
+  init: function () {
     var done = this.async();
     var that = this;
     var spawn = require('child_process').spawn;
-    var s1 = spawn( 'git', [ 'submodule -q foreach git pull -q origin master'], {cwd: this.sourceRoot() } );
-    s1.on('close', function() {
-      var s2 = spawn( 'git', [ 'reset', '--hard'], {cwd: that.sourceRoot() } );
-      s2.on('close', function() {
-        var s3 = spawn( 'git', [ 'pull'], {cwd: that.sourceRoot() } );
-        s3.on('close', function() {
+
+    var s1 = spawn('git', ['submodule -q foreach git pull -q origin master'], {cwd: this.sourceRoot()});
+    s1.on('error', function( err ){ console.log(err); throw err });
+    s1.on('close', function () {
+      var s2 = spawn('git', ['reset', '--hard'], {cwd: that.sourceRoot()});
+      s2.on('close', function () {
+        var s3 = spawn('git', ['pull'], {cwd: that.sourceRoot()});
+        s3.on('close', function () {
           done();
         });
       });
     });
   },
 
-  installAurelia: function() {
+  installAurelia: function () {
     var prompts = [];
-    var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+    var files = this.expandFiles('**/*', {cwd: this.sourceRoot(), dot: true});
     var ignores = [
       '.git',
       'LICENSE',
@@ -32,13 +34,17 @@ var Generator = module.exports = generators.Base.extend({
 
     this.log.writeln('Generating from ' + 'Aurelia Skeleton App'.cyan + ' v' + this.package.version.cyan + '...');
 
-    files.forEach(function(file) {
+    files.forEach(function (file) {
       if (ignores.indexOf(file) !== -1) {
         return;
       }
 
       this.copy(file, file);
     }, this);
+  },
+
+  executeNPMInstall: function () {
+    this.npmInstall();
   }
 });
 
