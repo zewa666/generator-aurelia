39255c7e07125588ad4d0a6fe26b3618434af1b3
Zewa <zewa666@gmail.com>
2015-04-17 19:36:41 +0200

    * fixed github download * it should automatically get the latest available version

--- app/index.js
+++ app/index.js
@@ -1,53 +1,34 @@
 var fs = require('fs');
-var path = require('path');
 var generators = require('yeoman-generator');
 
 var Generator = module.exports = generators.Base.extend({
   init: function () {
     var done = this.async();
     var that = this;
-    var spawn = require('child_process').spawn;
 
-    var s1 = spawn('git', ['submodule -q foreach git pull -q origin master'], {cwd: this.sourceRoot()});
-    s1.on('error', function( err ){ console.log(err); throw err });
-    s1.on('close', function () {
-      var s2 = spawn('git', ['reset', '--hard'], {cwd: that.sourceRoot()});
-      s2.on('close', function () {
-        var s3 = spawn('git', ['pull'], {cwd: that.sourceRoot()});
-        s3.on('close', function () {
-          done();
-        });
-      });
+    var ghdownload = require('download-github-repo')
+      , execFile = require('child_process').execFile;
+
+    console.log(this.destinationRoot());
+
+    ghdownload("aurelia/skeleton-navigation", this.destinationRoot(), function(err) {
+      if(err !== undefined) {
+        this.env.error(err);
+      } else {
+        console.log('Download complete');
+      }
+
+      done();
     });
   },
 
-  installAurelia: function () {
-    var prompts = [];
-    var files = this.expandFiles('**/*', {cwd: this.sourceRoot(), dot: true});
-    var ignores = [
-      '.git',
-      'LICENSE',
-      'README.md',
-    ];
-
-    this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-
-    this.log.writeln('Generating from ' + 'Aurelia Skeleton App'.cyan + ' v' + this.package.version.cyan + '...');
-
-    files.forEach(function (file) {
-      if (ignores.indexOf(file) !== -1) {
-        return;
-      }
-
-      this.copy(file, file);
-    }, this);
-  },
-
   executeNPMInstall: function () {
+    console.log('Executing NPM install');
     this.npmInstall();
   },
 
   runJSPM: function() {
+    console.log('Executing JSPM install');
     this.spawnCommand('jspm', ['install']);
   }
 });
