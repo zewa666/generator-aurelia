834291e00407a48afca9174472a14b9452e12d98
paul van bladel <paul.vanbladel@gmail.com>
2015-08-06 22:05:06 +0200

    option for skip-install

--- app/index.js
+++ app/index.js
@@ -1,35 +1,49 @@
 var fs = require('fs');
-var generators = require('yeoman-generator');
+var yeoman = require('yeoman-generator');
 
-var Generator = module.exports = generators.Base.extend({
+var Generator = module.exports = yeoman.generators.Base.extend({
+  constructor: function(){
+    yeoman.generators.Base.apply(this, arguments);
+    this.option('skip-install');
+  },
   init: function () {
     var done = this.async();
-    var that = this;
 
     var ghdownload = require('download-github-repo')
       , execFile = require('child_process').execFile;
 
-    console.log(this.destinationRoot());
+    this.log(this.destinationRoot());
 
     ghdownload("aurelia/skeleton-navigation", this.destinationRoot(), function(err) {
       if(err !== undefined) {
         this.env.error(err);
       } else {
-        console.log('Download complete');
+        this.log('Download complete');
       }
-
       done();
-    });
+    }.bind(this));
   },
 
   executeNPMInstall: function () {
-    console.log('Executing NPM install');
-    this.npmInstall();
+    if (!this.options['skip-install']){
+      this.log('Executing NPM install');
+      this.npmInstall();
+    }
+    else{
+    this.log('NPM install deliberately skipped');
+    };
   },
 
   runJSPM: function() {
-    console.log('Executing JSPM install');
-    this.spawnCommand('jspm', ['install']);
+
+    if (!this.options['skip-install']){
+      this.log('Executing JSPM install');
+      this.spawnCommand('jspm', ['install']);
+    }
+    else{
+      this.log('JSPM install deliberately skipped');
+    
+    };
   }
 });
 
