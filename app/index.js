dd4806578fc35e5403f745f99332480e3cceaf0d
Zewa <zewa666@gmail.com>
2015-08-17 16:56:24 +0200

    * search for latest available tag of the skeleton and install it

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var yeoman = require('yeoman-generator');
+var GitHubApi = require('github');
 
 var Generator = module.exports = yeoman.generators.Base.extend({
   constructor: function(){
     yeoman.generators.Base.apply(this, arguments);
     this.option('skip-install');
   },
+
   init: function () {
     var done = this.async();
 
@@ -14,13 +16,40 @@ var Generator = module.exports = yeoman.generators.Base.extend({
 
     this.log(this.destinationRoot());
 
-    ghdownload("aurelia/skeleton-navigation", this.destinationRoot(), function(err) {
-      if(err !== undefined) {
-        this.env.error(err);
-      } else {
-        this.log('Download complete');
+    var github = new GitHubApi({
+      // required
+      version: "3.0.0",
+      debug: false,
+      protocol: "https",
+      host: "api.github.com",
+      pathPrefix: "",
+      timeout: 5000,
+      headers: {
+        "user-agent": "Aurelia-Github-Loader"
       }
-      done();
+    });
+
+    github.repos.getTags({ user: 'aurelia', repo: 'skeleton-navigation', page: 1, per_page: 1 }, function(err, result) {
+      if(err !== undefined && err !== null) {
+        this.env.error('Failed to get latest release info');
+        return;
+      }
+
+      if(result.length < 1) {
+        this.env.error('No Release-Tags available');
+        return;
+      }
+      console.log('Downloading latest available release: ' + result[0].name);
+
+      // Kick off the repo download
+      ghdownload("aurelia/skeleton-navigation#" + result[0].name, this.destinationRoot(), function(err) {
+        if (err !== undefined && err !== null) {
+          this.env.error(err);
+        } else {
+          this.log('Download complete');
+          done();
+        }
+      }.bind(this));
     }.bind(this));
   },
 
@@ -28,10 +57,9 @@ var Generator = module.exports = yeoman.generators.Base.extend({
     if (!this.options['skip-install']){
       this.log('Executing NPM install');
       this.npmInstall();
+    } else {
+      this.log('NPM install deliberately skipped');
     }
-    else{
-    this.log('NPM install deliberately skipped');
-    };
   },
 
   runJSPM: function() {
@@ -39,11 +67,9 @@ var Generator = module.exports = yeoman.generators.Base.extend({
     if (!this.options['skip-install']){
       this.log('Executing JSPM install');
       this.spawnCommand('jspm', ['install']);
-    }
-    else{
+    } else{
       this.log('JSPM install deliberately skipped');
-    
-    };
+    }
   }
 });
 
