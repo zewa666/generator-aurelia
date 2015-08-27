cceaebd4fd5142b1de159f041e934f927e0a35d3
Zewa <zewa666@gmail.com>
2015-08-27 14:37:57 +0200

    * update only aurelia packages found in package.json

--- update/index.js
+++ update/index.js
@@ -2,19 +2,31 @@ var generators = require('yeoman-generator');
 var Promise = require('bluebird');
 var jspm = require('jspm');
 
+var jspmPackages = [];
+
 module.exports = generators.Base.extend({
+
+  getAllAureliaPackages: function() {
+    var conf = JSON.parse(this.readFileAsString(this.destinationPath('package.json')));
+
+    Object.keys(conf.jspm.dependencies).forEach( function(pkg) {
+      if(pkg.indexOf('aurelia-') === 0) {
+        jspmPackages.push(pkg);
+      }
+    });
+  },
 
   runJSPMUpdate: function() {
     var done = this.async();
-    var reps = ['aurelia-animator-css', 'aurelia-bootstrapper', 'aurelia-framework', 'aurelia-dependency-injection',
+    /*var reps = ['aurelia-animator-css', 'aurelia-bootstrapper', 'aurelia-framework', 'aurelia-dependency-injection',
       'aurelia-binding', 'aurelia-http-client', 'aurelia-router', 'aurelia-event-aggregator', 'aurelia-history-browser',
       'aurelia-loader-default', 'aurelia-loader', 'aurelia-metadata', 'aurelia-route-recognizer',
       'aurelia-templating-binding', 'aurelia-templating-resources', 'aurelia-templating-router', 'aurelia-templating',
-      'aurelia-logging', 'aurelia-task-queue', 'aurelia-history', 'aurelia-path'];
+      'aurelia-logging', 'aurelia-task-queue', 'aurelia-history', 'aurelia-path'];*/
 
     var allDone = [];
 
-    reps.forEach( function(repo) {
+    jspmPackages.forEach( function(repo) {
       allDone.push(jspm.install(repo));
     }.bind(this));
 
